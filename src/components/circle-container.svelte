fd7f1581b7e3d034b58f582c820229835dce64d8
RomperS <Oswaldolo663@gmail.com>
2024-09-02 15:34:50 -0500

    feat: creation of login page

---
+++ src/components/circle-container.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let src = '';
+  export let size = '100px';
+  export let background = 'transparent';
+  export let opacityImg = '1'
+</script>
+
+<style>
+  .circle-container {
+    width: var(--size);
+    height: var(--size);
+    border-radius: 50%;
+    background: var(--background);
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .circle-container img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: var(--opacity);
+  }
+</style>
+
+<div class="circle-container" style="--size: {size}; --background: {background}; --opacity: {opacityImg}">
+  <img src={src} alt="Circular Image" />
+</div>
